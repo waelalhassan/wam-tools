d30ada23d3b4f2d86e60c33af4210fa41833199d
wael alhassan <waelalhassan.fed@gmail.com>
2022-02-22 17:24:15 +0300

    m

--- src/pages/Base64Decode.js
+++ src/pages/Base64Decode.js
@@ -1,9 +1,47 @@
-import {useState} from "react";
+import { useRef, useState } from "react";
 
-const base64Decode = () => {
-    return (
-        <h1> base64Decode </h1>
-    )
-}
+const Base64Decode = () => {
+  const [getCode, setCode] = useState("");
+  const [getError, setError] = useState(false);
+  const RefInput = useRef(this);
 
-export default base64Decode;
+  const handledecode = () => {
+    function is64() {
+      if (RefInput.current.value == "" || RefInput.current.value.trim() == "")
+        return false;
+      try {
+        return btoa(atob(RefInput.current.value)) == RefInput.current.value;
+      } catch (err) {
+        return false;
+      }
+    }
+
+    if (is64()) {
+      setError((er) => (er = false));
+      setCode((code) => (code = atob(RefInput.current.value)));
+    } else {
+      setError((er) => (er = true));
+    }
+  };
+
+  return (
+    <section className="wrapper-base64-decode">
+      <div className="input">
+        <textarea
+          ref={RefInput}
+          placeholder="Enter your base64 code here ..."
+        ></textarea>
+        <button onClick={handledecode} type="button">
+          Decode
+        </button>
+      </div>
+      {getError ? (
+        <div>Invalid base64 code</div>
+      ) : (
+        <div className="output">{getCode}</div>
+      )}
+    </section>
+  );
+};
+
+export default Base64Decode;
