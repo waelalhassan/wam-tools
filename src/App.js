59365e59cadd7e621991d9684bd1becde0e0be57
wael alhassan <waelalhassan.fed@gmail.com>
2022-02-25 16:58:32 +0300

    m

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Base64Encode from "./pages/Base64Encode";
 import Base64Decode from "./pages/Base64Decode";
-import AgeCalculator from "./pages/AgeCalculator";
 import GeneratePassword from "./pages/GeneratePassword";
 import TextTool from "./pages/TextTool";
 import GenerateQRcode from "./pages/GenerateQRcode";
@@ -18,7 +17,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/base64-encode" element={<Base64Encode />} />
         <Route path="/base64-decode" element={<Base64Decode />} />
-        <Route path="/age-calculator" element={<AgeCalculator />} />
         <Route path="/generate-password" element={<GeneratePassword />} />
         <Route path="/text-tool" element={<TextTool />}/>
         <Route path="/generate-QRcode" element={<GenerateQRcode />}/>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,52 @@
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
       <Nav />
-      <main>
+      <main className="wrapper-home-page">
         <div className="container">
-          <div className="list-tools">
-            <ul className="d-flex d-wrap-row d-align-center">
-              <li className="col-3">
-                <Link to="/generate-password">Generate password</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/generate-QRcode">Generate QRcode</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/generate-shadow">Generate css shadow</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/generate-MD5-hash">Generate MD5 Hash</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/base64-decode">Base64 decode</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/base64-encode">Base64 encode</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/text-tool">Text tool</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/age-calculator">Age calculator</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/rgb-color-to-hex-color">RGB to hex Color</Link>
-              </li>
-              <li className="col-3">
-                <Link to="/hex-color-to-rgb-color">Hex color to RGB</Link>
-              </li>
-            </ul>
+          <div className="home-page">
+            <header className="p-t-4 p-b-4 text-center">
+              <h1>UR-TOOLS</h1>
+              <p>Important tools for most users</p>
+              <form>
+                <input className="w-80" type="text" placeholder="Search ..." />
+              </form>
+            </header>
+            <div className="list-tools">
+              <ul className="d-flex d-wrap-row d-align-center">
+                <li className="col-3">
+                  <Link to="/generate-password">Generate password</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/generate-QRcode">Generate QRcode</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/generate-shadow">Generate css shadow</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/generate-MD5-hash">Generate MD5 Hash</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/base64-decode">Base64 decode</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/base64-encode">Base64 encode</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/text-tool">Text tool</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/rgb-color-to-hex-color">RGB to hex Color</Link>
+                </li>
+                <li className="col-3">
+                  <Link to="/hex-color-to-rgb-color">Hex color to RGB</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </main>
